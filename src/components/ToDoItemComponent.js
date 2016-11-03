1c184908aeaf766eea9079fa0eecb5cafef0c647
kkolek <kkolek@future-processing.com>
2016-11-03 13:07:53 -0700

    Adding main components

---
+++ src/components/ToDoItemComponent.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import React from 'react';
+
+require('styles//ToDoItem.css');
+
+class ToDoItemComponent extends React.Component {
+  render() {
+    return (
+      <div className="todoitem-component">
+        Please edit src/components///ToDoItemComponent.js to update this component!
+      </div>
+    );
+  }
+}
+
+ToDoItemComponent.displayName = 'ToDoItemComponent';
+
+// Uncomment properties you need
+// ToDoItemComponent.propTypes = {};
+// ToDoItemComponent.defaultProps = {};
+
+export default ToDoItemComponent;
---
+++ src/components/ToDoListComponent.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import React from 'react';
+
+require('styles//ToDoList.css');
+
+class ToDoListComponent extends React.Component {
+  render() {
+    return (
+      <div className="todolist-component">
+        Please edit src/components///ToDoListComponent.js to update this component!
+      </div>
+    );
+  }
+}
+
+ToDoListComponent.displayName = 'ToDoListComponent';
+
+// Uncomment properties you need
+// ToDoListComponent.propTypes = {};
+// ToDoListComponent.defaultProps = {};
+
+export default ToDoListComponent;
